a2057ddcae27fa65b106ee0517c8d76beff8a29e
Andy <andybrummittps4@hotmail.com>
2020-12-31 22:02:20 +0000

    added browserslist and postcss

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 module.exports = {
     mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+    devtool: 'source-map',
     entry: './src/index.js',
     output: {
         filename: 'main.js',
@@ -41,7 +42,7 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
+                use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
             }
         ]
     },
