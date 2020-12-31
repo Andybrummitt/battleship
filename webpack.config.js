42819d531958dd3a82298d52d79e924685085ffc
Andy <andybrummittps4@hotmail.com>
2020-12-31 09:57:44 +0000

    fixed webpack config with css loader

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+                use: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
     },
