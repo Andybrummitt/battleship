160106c023b16c55b6d0245b033c61179e9521d6
Andy <andybrummittps4@hotmail.com>
2020-12-31 22:14:33 +0000

    testing safari added alert

--- src/index.js
+++ src/index.js
@@ -26,13 +26,15 @@ const addTitleStyles = () => {
 
 Window.onload = addTitleStyles();
 
+alert('testing')
+
 const createShipButtons = ships => ships.forEach(ship => {
     const btn = document.createElement('button');
     btn.textContent = ship.name;
     btn.className = 'ship-select-btn';
-    // btn.addEventListener('click', e => {
-    //     shipBtnClickListener(ship);
-    // });
+    btn.addEventListener('click', e => {
+        shipBtnClickListener(ship);
+    });
     domObj.shipBtnsDiv.appendChild(btn);
 });
 
